3d16b831316aa3c5d4ac466555558f22e4cba216
daya <changjian.zcj@antfin.com>
2018-11-03 20:36:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ open_filter_optimize="enable: true # filter_optimize"
 close_filter_optimize="enable: false # filter_optimize"
 
 # 开启filter_optimize
-sed -i "s/$close_filter_optimize/$open_filter_optimize/" _config.yml
+sed -i "" "s/$close_filter_optimize/$open_filter_optimize/" _config.yml
 
 hexo clean && hexo g && ./atom_plus.py
 chmod 755 public/atom.xml
@@ -13,4 +13,4 @@ hexo deploy
 
 
 # 关闭filter_optimize
-sed -i "s/$open_filter_optimize/$close_filter_optimize/" _config.yml
+sed -i "" "s/$open_filter_optimize/$close_filter_optimize/" _config.yml
